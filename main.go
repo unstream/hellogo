7e2a3f7b36bae41c2601608ec1055b4c389cffb0
Weichsel, Ingo <Ingo.Weichsel@adesso.de>
2022-03-25 00:59:27 +0100

    Add cobra

--- main.go
+++ main.go
@@ -5,10 +5,15 @@ import (
 	"fmt"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 	"github.com/unstream/hellogo/mandelbrot/boundary"
 	"net/http"
 	"os"
+	"strconv"
 )
+
+const version = "V1"
 
 // content holds our static web server content.
 //go:embed web/build
@@ -20,10 +25,38 @@ var contentHandler = echo.WrapHandler(http.FileServer(http.FS(content)))
 // The embedded files will all be in the '/static' folder so need to rewrite the request (could also do this with fs.Sub)
 var contentRewrite = middleware.Rewrite(map[string]string{"/*": "/web/build/$1"})
 
+var configPath string
+
+var rootCmd = &cobra.Command{
+	Use:   "main",
+	Short: "MandelbrotServer",
+	Long:  `Mandelbrot Server`,
+}
+
+func init() {
+	serverCmd := &cobra.Command{
+		Use:   "run",
+		Short: "server",
+		Run: func(cmd *cobra.Command, args []string) {
+		},
+	}
+	serverCmd.Flags().Int("port", 1138, "Port to run Application server on")
+	viper.BindPFlag("port", serverCmd.Flags().Lookup("port"))
+	viper.SetDefault("port", "8081")
+	rootCmd.AddCommand(serverCmd)
+}
+
 func main() {
+
+	viper.AutomaticEnv() // Automatically search for environment variables
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
 	name := "Mandelbrot-Server"
 	e := echo.New()
+	e.HideBanner = true
 	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
 		Format: fmt.Sprintf("\n%s | ${host} | ${time_custom} | ${status} | ${latency_human} | ${remote_ip} | ${bytes_in} bytes_in | ${bytes_out} bytes_out | ${method} | ${uri} ",
 			name,
@@ -40,7 +73,10 @@ func main() {
 	v1route := APIRoute.Group("/v1")
 	v1route.GET("/mandelbrot", boundary.MandelbrotGetHandler)
 	v1route.POST("/mandelbrot", boundary.MandelbrotPostHandler)
-
 	// firing up the server
-	e.Logger.Fatal(e.Start(":8081"))
+	var port = 8081
+	if viper.IsSet("port") {
+		port = viper.GetInt("port")
+	}
+	e.Logger.Fatal(e.Start(":" + strconv.Itoa(port)))
 }
